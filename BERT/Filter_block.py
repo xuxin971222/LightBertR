47f84f59a873f8aec0ef4774e46d319b0146fb4a
data <74396671+xuxin971222@users.noreply.github.com>
2022-04-28 12:14:15 +0800

    Add files via upload

---
+++ BERT/Filter_block.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn
+from BERT.res_net import LayerNorm
+
+class FilterLayer(nn.Module):
+    def __init__(self, args):
+        super(FilterLayer, self).__init__()
+
+        self.complex_weight = nn.Parameter(torch.randn(1, 103, args.hidden_size, 2, dtype=torch.float32) * 0.02)
+        self.out_dropout = nn.Dropout(args.hidden_dropout_prob)
+        self.LayerNorm = LayerNorm(args.hidden_size, eps=1e-12)
+
+    def forward(self, input_tensor):
+        # [batch, seq_len, hidden]
+        #sequence_emb_fft = torch.rfft(input_tensor, 2, onesided=False)  # [:, :, :, 0]
+        #sequence_emb_fft = torch.fft(sequence_emb_fft.transpose(1, 2), 2)[:, :, :, 0].transpose(1, 2)
+
+        batch, seq_len, hidden = input_tensor.shape
+        x = torch.fft.rfft(input_tensor, dim=1, norm='ortho')
+        weight = torch.view_as_complex(self.complex_weight)
+        x = x * weight
+        sequence_emb_fft = torch.fft.irfft(x, n=seq_len, dim=1, norm='ortho')
+        hidden_states = self.out_dropout(sequence_emb_fft)
+        hidden_states = self.LayerNorm(hidden_states + input_tensor)
+
+        return hidden_states
